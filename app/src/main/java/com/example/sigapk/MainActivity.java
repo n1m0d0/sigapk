5ebebbcb302f4b40576973732de8d1d1107387f1
n1m0d0 <vascojohn@gmail.com>
2021-09-15 16:01:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sigapk/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sigapk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(MainActivity.this, optionMenu.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
---
+++ app/src/main/java/com/example/sigapk/optionMenu.java
@@ -0,0 +1,14 @@
+package com.example.sigapk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class optionMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_menu);
+    }
+}
